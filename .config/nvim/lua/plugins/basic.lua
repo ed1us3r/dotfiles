19734d71e5d8883314459322802d3cb4752db709
ed1us3r <Ed1us3r@outlook.de>
2024-09-15 22:28:08 +0200

    [PLUGIN] 'AlexvZyl/nordic.nvim',

--- .config/nvim/lua/plugins/basic.lua
+++ .config/nvim/lua/plugins/basic.lua
@@ -9,4 +9,12 @@ return {
         vim.cmd([[colorscheme tokyonight]])
       end,
     },
+    {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require 'nordic' .load()
+    end
+  }
 }  
