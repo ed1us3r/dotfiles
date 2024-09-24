2a3528f0e2d501492d1fb772bbb28c22efa129d1
ed1us3r <Ed1us3r@outlook.de>
2024-09-24 22:45:58 +0200

    [EDIT]  relative numbers not only for vim also for neovim

--- .config/nvim/lua/plugins/startup.lua
+++ .config/nvim/lua/plugins/startup.lua
@@ -1,13 +1,6 @@
 return{
   { url = "git@github.com:mong8se/actually.nvim.git" },
   {
-    "startup-nvim/startup.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
-    config = function()
-      require "startup".setup()
-    end
-  },
-  {
     "nvim-neo-tree/neo-tree.nvim",
       opts = {
         filesystem = {
