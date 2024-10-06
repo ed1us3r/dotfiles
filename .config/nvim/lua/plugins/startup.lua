b0ca580e9b58c1a15692a7889d7c6dd3cd90af5f
ed1us3r <Ed1us3r@outlook.de>
2024-10-06 17:13:01 +0200

    [NVIM] Add nvim function

--- .config/nvim/lua/plugins/startup.lua
+++ .config/nvim/lua/plugins/startup.lua
@@ -6,6 +6,12 @@ return{
         filesystem = {
           filtered_items = {
       visible = true,
+      dependencies = {
+      	"nvim-lua/plenary.nvim",
+     	"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+     	"MunifTanjim/nui.nvim",
+      	-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
       show_hidden_count = true,
       hide_dotfiles = false,
       hide_gitignored = true,
