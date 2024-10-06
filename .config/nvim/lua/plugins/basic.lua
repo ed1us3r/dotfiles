3cf53d18cfdf9005720f9ce38d7e8bc08da79d26
ed1us3r <Ed1us3r@outlook.de>
2024-10-06 18:32:22 +0200

    [NVIM] Add nvim function

--- .config/nvim/lua/plugins/basic.lua
+++ .config/nvim/lua/plugins/basic.lua
@@ -16,5 +16,24 @@ return {
     config = function()
         require 'nordic' .load()
     end
-  }
+    },
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+},
+{ "git@github.com/nvim-lua/plenary.nvim"}
 }  
