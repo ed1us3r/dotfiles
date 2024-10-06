33cd55f4728aad255206a83370280db08d07aacb
ed1us3r <Ed1us3r@outlook.de>
2024-10-06 23:15:49 +0200

    [NVIM] add more compatability

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -5,6 +5,13 @@ return {
     priority = 1000,
     config = function()
         require 'nordic' .load()
+	require('nordic').setup({
+    	transparent = {
+        	-- Enable transparent background.
+        	bg = true,
+        	-- Enable transparent background for floating windows.
+        	float = true,
+    },
     end
   }
 
